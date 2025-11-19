2109ee50bcd4b80dc5bc0fbc23eaaf1312c968bc
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-11-19 14:08:55 +0100

    Refactored read_value long lines

--- src/generator.rs
+++ src/generator.rs
@@ -631,10 +631,14 @@ impl<'a> GameLogGenerator<'a> {
 
     // Read value from cell in format B15
     fn read_value(&mut self, sheet: &str, column: usize, row: usize) -> Result<Data, XlsxError> {
-        if let Some(range) = self.sheets.get(sheet)
-            && let Some(value) = range.get((row.saturating_sub(1), column.saturating_sub(1)))
-        {
-            return Ok(value.clone());
+        let cell = (row.saturating_sub(1), column.saturating_sub(1));
+
+        if let Some(range) = self.sheets.get(sheet) {
+            let sheet_value = range.get(cell);
+
+            if let Some(value) = sheet_value {
+                return Ok(value.clone());
+            }
         }
 
         Ok(Data::Empty)
